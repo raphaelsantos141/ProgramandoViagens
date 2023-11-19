176a274a1b5169aedbbb681ab2b582168e0bb960
Raphael <raphael.asantos@outlook.com>
2023-11-19 19:45:49 -0300

    Adicionado galeria

--- home.js
+++ home.js
@@ -157,3 +157,19 @@ carrosel.addEventListener("scroll", infiniteScroll);
 imagens_carrosel.addEventListener("mouseenter", () => clearTimeout(timeoutId));
 imagens_carrosel.addEventListener("mouseleave", autoPlay);
 
+let slides = document.querySelectorAll('.slide-container');
+let index = 0;
+
+function next(){
+    slides[index].classList.remove('active');
+    index = (index + 1) % slides.length;
+    slides[index].classList.add('active');
+}
+
+function prev(){
+    slides[index].classList.remove('active');
+    index = (index - 1 + slides.length) % slides.length;
+    slides[index].classList.add('active');
+}
+
+setInterval(next, 7000);
